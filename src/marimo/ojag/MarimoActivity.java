31fd49299a3f4b3c7ae81c0f55aaa9dd5657072c
cota2n <cota2n@gmail.com>
2012-02-18 20:03:51 +0900

    first

---
+++ src/marimo/ojag/MarimoActivity.java
@@ -0,0 +1,13 @@
+package marimo.ojag;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MarimoActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
