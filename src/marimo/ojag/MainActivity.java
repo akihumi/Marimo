315e444b0c80bd01c69d7cc0c3fb5a88b0417069
cota2n <cota2n@gmail.com>
2012-02-19 17:25:02 +0900

    add Menu

--- src/marimo/ojag/MainActivity.java
+++ src/marimo/ojag/MainActivity.java
@@ -1,7 +1,33 @@
 package marimo.ojag;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.mainmenu, menu);
+		return true;
+	}
+	public boolean onOptionsItemSlected(MenuItem item){
+		switch(item.getItemId()){
+		case R.id.tomanager:
+			Intent intent = new Intent(MainActivity.this , Manager.class);
+			startActivity(intent);
+			return true;
+		default :
+			return super.onOptionsItemSelected(item);
+		}
+	}
 }
--- src/marimo/ojag/MarimoActivity.java
+++ src/marimo/ojag/MarimoActivity.java
@@ -1,13 +1,29 @@
 package marimo.ojag;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MarimoActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.opening);
+        
+        Button start = (Button)findViewById(R.id.start);
+        start.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MarimoActivity.this , MainActivity.class);
+				startActivity(intent);
+			}
+		});
     }
+    
+    
+    
+    
 }
